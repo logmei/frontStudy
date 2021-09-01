74f4eb2b368b40bc2d0552458d1e3d34db123133
logmei <522844663@qq.com>
2021-09-01 09:26:09 +0800

    websocket

--- node/websocket/server.js
+++ node/websocket/server.js
@@ -3,6 +3,13 @@ wss = new WebSocketServer({ port: 8181 });
 wss.on('connection', function (ws) {
     console.log('client connected');
     ws.on('message', function (message) {
-        console.log(message);
+        console.log('message',message);
     });
+    const param = {payload:{
+      deviceSerial:'G00287022',equipmentId:'2',id:'871',locationId:'1',
+      locationName:'大门A',streamToken:'at.2hnuke9bbkd6o3wt37om85am96g5fbof-9nyicekco7-0idzh3i-zlbkrsdkc',warningStatus:'INIT'},type:'WARNING',userId:3}
+    ws.send(JSON.stringify({type: 'REGISTER', userId: 3}))
+    // setInterval(()=>{
+    //   ws.send(JSON.stringify(param))
+    // },10000)
 });
